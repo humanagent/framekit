ae42e0fb396ab3e90072e114bb17a27471c2064a
fabri <fguespe@gmail.com>
2024-11-22 16:11:19 -0300

    deploy

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  staticPageGenerationTimeout: 1000,
+};
+
+export default nextConfig;
